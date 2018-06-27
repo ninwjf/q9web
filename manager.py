e9b808563b1806e05daa2c63f8b30d94d8a0c11a
ninwjf <xyxwjf@qq.com>
2018-06-27 15:09:56 +0800

    freeswith 修改

--- manager.py
+++ manager.py
@@ -1,5 +1,6 @@
 from app import create_app
 from app.mobile import mobile
+from app.freeswitch import freeswitch
 from config import DEBUG
 
 global DEBUG
@@ -8,7 +9,7 @@ app = create_app()  # 创建APP
 
 # 注册蓝图
 app.register_blueprint(mobile, url_prefix='/mobile')    # 手机APP相关
-app.register_blueprint(mobile, url_prefix='/freeswitch')    # freeswitch相关
+app.register_blueprint(freeswitch, url_prefix='/freeswitch')    # freeswitch相关
 
 if __name__ == '__main__':
     app.run()
